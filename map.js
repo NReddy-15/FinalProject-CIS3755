da7fadfd9637ca35cb5712adc825324db707a6d2
Elossiem <sunnykeyly@gmail.com>
2026-04-23 12:44:24 -0400

    Modified Code
    Modified a few stuff

--- map.js
+++ map.js
@@ -1,6 +1,14 @@
-//grab our canvas 
-let svg = d3.select("#canvas");
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-//set the width and height
-svg.attr('width', 500)
-    .attr('height', 500)
+const svg = d3.select("svg");
+console.log("D3 loaded:", d3);
+
+d3.csv("top_1000_most_swapped_books.csv")
+    .then(function (data) {
+
+        console.log("Rows before filtering:", data.length)
+        console.log(data);
+    });
+    
+
+
