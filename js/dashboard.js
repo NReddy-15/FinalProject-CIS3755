e8b2441f3ed72908437cdf9663e40e5afe49e6fd
Victoria D. <tovictoriadao@gmail.com>
2026-04-27 23:08:28 -0400

    Victoria : Ensure that the cleaned datasets are loading properly => Uncomment the JS functions that we were not using (temporarily) => Load the CSVs

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,16 @@
-
-
 //grab our canvas 
 let svg = d3.select("#canvas");
+
+var readerAgesData = await d3.csv("../datasets/dashboard/Combined_BookRec_GenreToAge.csv")
+var readerGenderData = await d3.csv("../datasets/dashboard/Combined_GlobalReader_GenderToGender.csv")
+
 
 //set the width and height
 svg.attr('width', 500)
     .attr('height', 500)
 
 
-// Clean the data
+console.log(readerAgesData)
+console.log(readerGenderData)
 
 
