7a948b68c842f579ba114a42e01f305572ee1843
Victoria D. <tovictoriadao@gmail.com>
2026-04-27 23:14:28 -0400

    Victoria : Testing datasets being added - Part 2

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,15 @@
-//grab our canvas 
-let svg = d3.select("#canvas");
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-var readerAgesData = await d3.csv("../datasets/dashboard/Combined_BookRec_GenreToAge.csv")
-var readerGenderData = await d3.csv("../datasets/dashboard/Combined_GlobalReader_GenderToGender.csv")
+var readerAgesData = await d3.csv("../datasets/dashboard/Combined_BookRec_GenreToAge.csv");
+var readerGenderData = await d3.csv("../datasets/dashboard/Combined_GlobalReader_GenreToGender.csv");
 
+// //grab our canvas 
+// let svg = d3.select("#canvas");
 
-//set the width and height
-svg.attr('width', 500)
-    .attr('height', 500)
+// //set the width and height
+// svg.attr('width', 500)
+//     .attr('height', 500)
+
 
 
 console.log(readerAgesData)
